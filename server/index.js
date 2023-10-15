b5ed2295f2fe84d9c2c29238cce7138d3e21c14f
Dhanekula Yaswanth <111735437+DhanekulaYaswanth@users.noreply.github.com>
2023-10-15 14:33:22 +0530

    added published url

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ const app = express()
 const buildPath = path.join(__dirname+"/public")
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://kcpsarpschool.org');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const allowedOrigins = [
   'https://dhanekulayaswanth.github.io',
   'http://localhost:3000',
@@ -28,15 +35,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://kcpsarpschool.org');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 
 
